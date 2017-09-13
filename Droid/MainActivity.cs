b0189d9b24995bbe88c41e9359e20e3a3ca29cc5
KumakoT123 <edwintang10@yahoo.com.sg>
2017-09-13 13:15:48 +1000

    Add FCM notifications
    Add:
    FCM notifications
    
    Note: There will be an output for the notification that is sent to the application.

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -8,13 +8,28 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 
+//--------Google Play Services--------
+using Android.Gms.Common;
+
+//--------Firebase Messaging--------
+using Firebase;
+using Firebase.Messaging;
+using Firebase.Iid;
+using Android.Util; //---Adds logging functionality to transactions
+
 namespace Aria.Droid
 {
     [Activity(Label = "Aria.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
-        protected override void OnCreate(Bundle bundle)
+		//google play services walkthrough
+		TextView msgText;
+        const string TAG = "MainActivity";
+
+        /*protected override void OnCreate(Bundle bundle)
         {
+            
+
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
@@ -23,6 +38,60 @@ namespace Aria.Droid
             global::Xamarin.Forms.Forms.Init(this, bundle);
 
             LoadApplication(new App());
-        }
+        }*/
+
+		protected override void OnCreate(Bundle bundle)
+		{
+            Log.Debug(TAG, "google app id: " + Resource.String.google_app_id);
+
+			base.OnCreate(bundle);
+			SetContentView(Resource.Layout.Main);
+
+
+
+			msgText = FindViewById<TextView>(Resource.Id.msgText);
+
+			if (Intent.Extras != null)
+			{
+				foreach (var key in Intent.Extras.KeySet())
+				{
+					var value = Intent.Extras.GetString(key);
+					Log.Debug(TAG, "Key: {0} Value: {1}", key, value);
+				}
+			}
+
+			IsPlayServicesAvailable();
+
+			var logTokenButton = FindViewById<Button>(Resource.Id.logTokenButton);
+
+            logTokenButton.Click += delegate
+            {
+                Log.Debug(TAG, "InstanceID token: " + FirebaseInstanceId.Instance.Token);
+			};
+		}
+
+		public bool IsPlayServicesAvailable()
+		{
+			int resultCode = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this);
+			if (resultCode != ConnectionResult.Success)
+			{
+				if (GoogleApiAvailability.Instance.IsUserResolvableError(resultCode))
+					msgText.Text = GoogleApiAvailability.Instance.GetErrorString(resultCode);
+				else
+				{
+					msgText.Text = "This device is not supported";
+					Finish();
+				}
+				return false;
+			}
+			else
+			{
+				msgText.Text = "Google Play Services is available.";
+				return true;
+			}
+		}
+
+
     }
+
 }
---
+++ Droid/MyFirebaseMessagingService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Media;
+using Android.Util;
+using Firebase.Messaging;
+
+namespace Aria.Droid
+{
+	[Service]
+    //Intent filter must be declared so that new FCM messages are directed to FirebaseMessaging Service
+	[IntentFilter(new[] { "com.google.firebase.MESSAGING_EVENT" })]
+	public class MyFirebaseMessagingService : FirebaseMessagingService
+	{
+		const string TAG = "MyFirebaseMsgService";
+
+        //Method: Extracts the message content from the passed-in RemoteMessage object by calling the GetNotification method
+		public override void OnMessageReceived(RemoteMessage message)
+		{
+			Log.Debug(TAG, "From: " + message.From);
+			Log.Debug(TAG, "Notification Message Body: " + message.GetNotification().Body);
+		}
+	}
+}
